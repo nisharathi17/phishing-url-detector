2c589cbab40aae847ce2d758ac1cc5e4f44a34da
nisharathi17 <17nisharathi@gmail.com>
2026-07-19 23:48:52 +0530

    Integrate trained model into FastAPI prediction endpoint

--- src/app.py
+++ src/app.py
@@ -1,6 +1,18 @@
+from urllib import request
+
 from fastapi import FastAPI
 from pydantic import BaseModel
+import joblib
+import pandas as pd
+from pathlib import Path
 
+from src.feature_extractor import extract_features
+BASE_DIR = Path(__file__).resolve().parent.parent
+MODEL_PATH = BASE_DIR / "models" / "random_forest.pkl"
+
+model = joblib.load(MODEL_PATH)
+
+print("Model loaded successfully!")
 app = FastAPI()
 
 
@@ -17,6 +29,18 @@ def home():
 
 @app.post("/predict")
 def predict(request: URLRequest):
+
+    features = extract_features(request.url)
+
+    X = pd.DataFrame([features])
+
+    prediction = model.predict(X)[0]
+    probabilities = model.predict_proba(X)[0]
+
+    confidence = float(max(probabilities))
+
     return {
-        "received_url": request.url
+    "url": request.url,
+    "prediction": prediction,
+    "confidence": round(confidence * 100, 2)
     }
